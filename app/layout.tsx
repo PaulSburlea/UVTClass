79e7ea5d2960559436edb3d23d2f21ab82bf3db0
PaulSburlea <benjamin.sburlea02@e-uvt.ro>
2025-06-04 15:15:54 +0300

    added custom favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <head />
+        <head>
+          <meta name="apple-mobile-web-app-title" content="UVTClass" />
+        </head>
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <header className="flex justify-end items-center p-4 gap-4 h-16">
               <SignedOut>
